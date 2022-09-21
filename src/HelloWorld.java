995fca2eb3be3ba1cf39aee3f23b8309e580b72a
1E10 Laptop <sample@bths.edu>
2022-09-21 12:15:56 -0400

    added user input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello world!");
+        String userResponse = scan.nextLine();
     }
 }
